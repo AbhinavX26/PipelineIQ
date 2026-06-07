c5bfb55722ecf8eb114d4bb1a9114895a5fb4f06
AbhinavX26 <abhinav.nair916@gmail.com>
2026-06-08 01:51:16 +0530

    Add files via upload

---
+++ .next/dev/server/app/api/incidents/route.js
@@ -0,0 +1,9 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/incidents/route.js")
+R.c("server/chunks/1fmp_This Is Me - God_Projects_Useful_HackBaroda_app_api_incidents_route_ts_1rghfz-._.js")
+R.c("server/chunks/0rzy_next_0q332ta._.js")
+R.c("server/chunks/0rzy_zod_v4_1x2qclj._.js")
+R.c("server/chunks/0rzy_11glgp2._.js")
+R.c("server/chunks/[root-of-the-server]__1sc9h8t._.js")
+R.c("server/chunks/0kog_Useful_HackBaroda__next-internal_server_app_api_incidents_route_actions_1g_48he.js")
+R.m("[project]/OneDrive/Desktop/This Is Me - God/Projects/Useful/HackBaroda/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/OneDrive/Desktop/This Is Me - God/Projects/Useful/HackBaroda/app/api/incidents/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/OneDrive/Desktop/This Is Me - God/Projects/Useful/HackBaroda/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/OneDrive/Desktop/This Is Me - God/Projects/Useful/HackBaroda/app/api/incidents/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
